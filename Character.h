b4d6dd51b591157c2c3f0938742614fdc7aea8ba
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2023-10-30 11:32:31 -0400

    Update to character constructor in header file

--- Character.h
+++ Character.h
@@ -12,7 +12,7 @@ class Character : public BaseCharacter
         virtual Vector2 GetScreenPos() override;
         Rectangle getWeaponRect() {return weaponCollisionRec;};
         float GetHealth() const { return health; };
-        void TakeDamage(float damage);
+        void TakeDamage(float damage, float deltaTime = 0.0f);
         bool damaged = false;
     private:
         Rectangle weaponCollisionRec{};
@@ -20,6 +20,7 @@ class Character : public BaseCharacter
         int winHeight{};
         Texture2D weapon = LoadTexture("characters/weapon_sword.png");
         float health{100.f};
+        float invTimer = 3.0f;
 };
 
 #endif
